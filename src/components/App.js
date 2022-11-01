1e9ea8aac6f4a1cad0a9e857b9fcca87936a8e8c
James Howat <97859901+jbhowat@users.noreply.github.com>
2022-10-31 21:06:13 -0600

    webpack and front end boilerplate is done

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world, I am Franler!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
